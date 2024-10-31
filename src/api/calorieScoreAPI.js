65db81425cf466fa3428c72ffa37d563c929e741
임성균 <97024187+nuyggnoes@users.noreply.github.com>
2024-11-01 00:07:14 +0900

    Feat: 칼로리 스코어 화면

--- src/api/calorieScoreAPI.js
+++ src/api/calorieScoreAPI.js
@@ -51,3 +51,92 @@ export const getCalorieAndScore = async () => {
     console.log(error);
   }
 };
+
+export const getCalorieScoreRecords = async () => {
+  try {
+    // const response = await apiClient.get(`${BASE_URL}/api/kcal/change`);
+    // console.log(JSON.stringify(response.data, null, 2));
+    // return response.data.data;
+    const fakeResponse = await new Promise((resolve) => {
+      setTimeout(() => {
+        resolve({
+          status: 'OK',
+          data: [
+            {
+              id: 30,
+              userId: 2,
+              name: '지웅',
+              date: [2024, 10, 29],
+              score: 500,
+            },
+            {
+              id: 32,
+              userId: 2,
+              name: '지웅',
+              date: [2024, 9, 28],
+              score: 200,
+            },
+            {
+              id: 35,
+              userId: 2,
+              name: '지웅',
+              date: [2024, 8, 28],
+              score: 190,
+            },
+            {
+              id: 33,
+              userId: 2,
+              name: '지웅',
+              date: [2024, 7, 28],
+              score: 20,
+            },
+            {
+              id: 34,
+              userId: 2,
+              name: '지웅',
+              date: [2024, 6, 28],
+              score: 0,
+            },
+          ],
+          message: 'SUCCESS',
+        });
+      }, 500);
+    });
+    const data = {
+      labels: fakeResponse?.data.map(
+        (item) =>
+          `${String(item.date[1]).padStart(2, '0')}.${String(item.date[2]).padStart(2, '0')}`,
+      ),
+      datasets: [
+        {
+          data: fakeResponse?.data.map((item) => item.score),
+          // strokeWidth: 4, // optional
+        },
+      ],
+      legend: ['스코어 변화'], // optional
+    };
+    console.log(JSON.stringify(data, null, 2));
+
+    // console.log(JSON.stringify(fakeResponse, null, 2));
+    // return fakeResponse.data;
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getCalorieScoreRank = async () => {
+  try {
+    const response = await apiClient.get(`${BASE_URL}/api/kcal/rank`);
+    const prefix = response?.data?.data;
+    const returnData = {
+      name: prefix.name || '정채원',
+      score: prefix.score || 868,
+      rank: prefix.rank || 4,
+      perRank: prefix.perRank || 20,
+    };
+    return returnData;
+  } catch (error) {
+    console.log(error);
+  }
+};
--- src/components/units/CalorieScoreRanking.jsx
+++ src/components/units/CalorieScoreRanking.jsx
@@ -3,121 +3,127 @@ import { View, Text, StyleSheet } from 'react-native';
 import { globalStyles } from '../../styles/globalStyles';
 import * as Progress from 'react-native-progress';
 import { getDate, getMonth, getYear } from 'date-fns';
+import { useEffect, useState } from 'react';
+import { getCalorieScoreRank } from '../../api/calorieScoreAPI';
 
-export default function CalorieScoreRanking({
-    username,
-    calorieScore,
-    totalRanking,
-    friendRanking,
-}) {
-    const today = new Date();
-    year=getYear(today);
-    month=getMonth(today)+1;
-    date=getDate(today);
-    return (
-        <MainContainer style={styles.container}>
-            <View style={styles.titleContainer}>
-                <Text style={styles.titleText}>Calorie Score</Text>
-            
-            </View>
-            <View style={styles.contentContainer}>
-                <Text style={styles.dateText}>
-                    {year}년 {month}월 {date}일
-                </Text>
+export default function CalorieScoreRanking({}) {
+  const [username, setUsername] = useState();
+  const [calorieScore, setCalorieScore] = useState();
+  const [rankPercentage, setRankPercentage] = useState();
+  const [rank, setRank] = useState();
 
-                <View style={styles.circleContainer}>
-                    <Progress.Circle
-                        size={120}
-                        progress={totalRanking / 100}  
-                        showsText={false}
-                        color={globalStyles.mainColor}
-                        unfilledColor="#e0e0e0"
-                        borderWidth={0}
-                        thickness={10}
-                    />
-                    <Text style={styles.calorieScore}>{calorieScore}점</Text>
-                </View>
-                    
-                <Text style={styles.usernameText}>
-                    {username} 님
-                </Text>
-                <Text style={styles.totalRanking}>
-                    오늘 확인 | 상위 {totalRanking}%
-                </Text>
-                <Text style={styles.friendRanking}>
-                    친구랭킹 {friendRanking}위
-                </Text>
-            </View>
-        </MainContainer>
-    );
+  const fetchRankData = async () => {
+    const response = await getCalorieScoreRank();
+    console.log(response);
+    setUsername(response.name);
+    setCalorieScore(response.score);
+    setRankPercentage(response.perRank || 0);
+    setRank(response.rank || 0);
+  };
+  useEffect(() => {
+    fetchRankData();
+  }, []);
+  const today = new Date();
+  year = getYear(today);
+  month = getMonth(today) + 1;
+  date = getDate(today);
+  return (
+    <MainContainer style={styles.container}>
+      <View style={styles.titleContainer}>
+        <Text style={styles.titleText}>Calorie Score</Text>
+      </View>
+      <View style={styles.contentContainer}>
+        <Text style={styles.dateText}>
+          {year}년 {month}월 {date}일
+        </Text>
+
+        <View style={styles.circleContainer}>
+          <Progress.Circle
+            size={120}
+            progress={rankPercentage ? rankPercentage / 100 : 0}
+            showsText={false}
+            color={globalStyles.mainColor}
+            unfilledColor="#e0e0e0"
+            borderWidth={0}
+            thickness={10}
+          />
+          <Text style={styles.calorieScore}>{calorieScore}점</Text>
+        </View>
+
+        <Text style={styles.usernameText}>{username} 님</Text>
+        <Text style={styles.totalRanking}>
+          오늘 확인 | 상위 {rankPercentage}%
+        </Text>
+        <Text style={styles.friendRanking}>친구랭킹 {rank}위</Text>
+      </View>
+    </MainContainer>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        backgroundColor: globalStyles.backgroundColor,
-        padding: 20,
-        
-    },
-    titleContainer:{
-        backgroundColor: globalStyles.mainColor,
-        borderRadius: 15,
-        marginBottom: 20,
-    },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: globalStyles.backgroundColor,
+    padding: 20,
+  },
+  titleContainer: {
+    backgroundColor: globalStyles.mainColor,
+    borderRadius: 15,
+    marginBottom: 20,
+  },
 
-    titleText: {
-        fontSize: 30,
-        color: "white",
-        paddingHorizontal: 20,
-        paddingVertical: 10,
-        textAlign: 'left',
-        width: '100%',
-        fontWeight: 'bold',
+  titleText: {
+    fontSize: 30,
+    color: 'white',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    textAlign: 'left',
+    width: '100%',
+    fontWeight: 'bold',
+  },
 
-    },
+  contentContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+  },
 
-    contentContainer: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        flex: 1,
-    },
+  dateText: {
+    fontSize: 16,
+    color: globalStyles.textColor,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
 
-    dateText: {
-        fontSize: 16,
-        color: globalStyles.textColor,
-        fontWeight: 'bold',
-        marginBottom: 10,
-    },
+  circleContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 20,
+  },
 
-    circleContainer: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginVertical: 20,
-    },
+  calorieScore: {
+    position: 'absolute',
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: globalStyles.mainColor,
+  },
 
-    calorieScore: {
-        position: 'absolute',
-        fontSize: 24,
-        fontWeight: 'bold',
-        color: globalStyles.mainColor,
-    },
+  usernameText: {
+    fontSize: 20,
+    color: globalStyles.secondaryTextColor,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
 
-    usernameText: {
-        fontSize: 20,
-        color: globalStyles.secondaryTextColor,
-        fontWeight: 'bold',
-        marginBottom: 10,
-    },
+  totalRanking: {
+    fontSize: 18,
+    color: globalStyles.mainColor,
+    marginBottom: 10,
+  },
 
-    totalRanking: {
-        fontSize: 18,
-        color: globalStyles.mainColor,
-        marginBottom: 10,
-    },
-
-    friendRanking: {
-        fontSize: 18,
-        color: '#42A5F5',
-    },
+  friendRanking: {
+    fontSize: 18,
+    color: '#42A5F5',
+  },
 });
